686b8c8c716a2866bb098adb25cd1871aabede95
GoranTurundzov <77289944+GoranTurundzov@users.noreply.github.com>
2021-02-14 20:20:37 +0100

    macedonian language added

--- advancedJS/numToWords/script/script.js
+++ advancedJS/numToWords/script/script.js
@@ -159,7 +159,7 @@ function numToWord(){
             output.value =  billionNum(valueInput.value); 
             break;
         default:
-            output.value = `Please enter a valied number between 0 and 9999999999`
+            output.value = `Please enter a valied number between 0 and 9999999999 `
             break;
             
     
@@ -170,8 +170,7 @@ function numToWord(){
 
 
 buttonMkd.addEventListener(`click` , function(){
-    
-    output.value = `WORK IN PROGRESS`
+     printNumberMkd();
 })
 buttonEng.addEventListener(`click` , function(){
     numToWord()
@@ -183,5 +182,107 @@ valueInput.addEventListener(`keydown` , function(){
 })
 valueInput.addEventListener(`keyup` , function(){
     valueInput.style.backgroundColor = ` white`
-    numToWord()
 })
+
+
+
+let singleDigitMkd = ["", "еден", "два", "три", "четири", "пет", "шест", "седум", "осум", "девет"];
+let teensMkd =  ["десет", "единаесет", "дванаесет", "тринаесет", "четиринаесет", "петнаесет", "шестнаесет", "седумнаесет", "осумнаесет", "деветнаесет"];
+let dubleDigitsMkd = ["", "десет", "дваесет", "триесет", "четириесет", "педесет", "шеесет", "седумдесет", "осумдесет", "деведесет"];
+let multiDigitsMkd = [ "сто", "стотини", "илјади", "милион" , "милјарда" ];
+let iregularsMkd = ["двесте" , "триста" , "илјада"];
+
+function singleDIgitNumMkd(number){
+    return singleDigitMkd[number]
+}
+function dubleDigitsNumMkd(number){
+    return number[0] == 0 ? singleDigitMkd[number[1]]
+    : number[0] == 1 ? teensMkd[number[1]]
+    : number[0] >= 2 && number[1] == 0 ? dubleDigitsMkd[number[0]]
+    : `${dubleDigitsMkd[number[0]]} и ${singleDigitMkd[number[1]]}`
+}
+function tripleDIgitNumMkd(number){
+    return number[0] == 0 ? `${dubleDigitsNumMkd(`${number[1]}${number[2]}`)}`
+    : number[0] == 1 ? `${multiDigitsMkd[0]} ${dubleDigitsNumMkd(`${number[1]}${number[2]}`)}`
+    : number[0] == 2 ? `${iregularsMkd[0]} ${dubleDigitsNumMkd(`${number[1]}${number[2]}`)}`
+    : number[0] == 3 ?  `${iregularsMkd[1]} ${dubleDigitsNumMkd(`${number[1]}${number[2]}`)}`
+    : `${singleDigitMkd[number[0]]}${multiDigitsMkd[1]} ${dubleDigitsNumMkd(`${number[1]}${number[2]}`)}`
+}
+function fourDigitsMkd(number){
+    return number[0] == 0 ?  tripleDIgitNumMkd(`${number[1]}${number[2]}${number[3]}`)
+    : number[0] == 1 ? `${iregularsMkd[2]} ${tripleDIgitNumMkd(`${number[1]}${number[2]}${number[3]}`)}`
+    : `${singleDigitMkd[number[0]]} ${multiDigitsMkd[2]} ${tripleDIgitNumMkd(`${number[1]}${number[2]}${number[3]}`)}`
+}
+function fiveDigitsMkd(number){
+    return number[0] == 0 ?  fourDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}`)
+    : `${dubleDigitsNumMkd(`${number[0]}${number[1]}`)} ${multiDigitsMkd[2]} ${tripleDIgitNumMkd(`${number[2]}${number[3]}${number[4]}`)}`
+}
+function sixDigitsMkd(number){
+    return number[0] == 0 ? fiveDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}`)
+    :`${tripleDIgitNumMkd(`${number[0]}${number[1]}${number[2]}`)} ${multiDigitsMkd[2]} ${tripleDIgitNumMkd(`${number[3]}${number[4]}${number[5]}`)}`
+}
+function sevenDigitsMkd(number){
+    return number[0] == 0 ? sixDigitsMkd(`${number[0]}${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}`)
+    : number[0] == 1 ? `${singleDigitMkd[number[0]]} ${multiDigitsMkd[3]} ${sixDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}`)}`
+    :`${singleDigitMkd[number[0]]} ${multiDigitsMkd[3]}и ${sixDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}`)}`
+}
+function eightDigitsMkd(number){
+    return number[0] == 0 ? sevenDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}`)
+    : `${dubleDigitsNumMkd(`${number[0]}${number[1]}`)} ${multiDigitsMkd[3]}и ${sixDigitsMkd(`${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}`)} `
+   
+}
+function nineDigitsMkd(number){
+    return  number[0] == 0  ?  eightDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}${number[8]}`)
+    : `${tripleDIgitNumMkd(`${number[0]}${number[1]}${number[2]}`)} ${multiDigitsMkd[3]}и ${sixDigitsMkd(`${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}${number[8]}`)}`
+}
+function tenDigitsMkd(number){
+    return number[0] == 0 ? nineDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}${number[8]}${number[9]}`)
+    : number[0] == 1 ? `${singleDIgitNumMkd(number[0])} ${multiDigitsMkd[4]} ${nineDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}${number[8]}${number[9]}`)}`
+    : `${singleDIgitNumMkd(number[0])} ${multiDigitsMkd[4]}и ${nineDigitsMkd(`${number[1]}${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}${number[7]}${number[8]}${number[9]}`)}`
+}
+
+
+function printNumberMkd(){
+    let number = valueInput.value
+    if(number == 0){
+        output.value = `Нула`
+    }else{
+        switch(valueInput.value.length){
+            case 1:
+                output.value = singleDIgitNumMkd(valueInput.value);
+                break;
+            case 2:
+                output.value = dubleDigitsNumMkd(valueInput.value);
+                break;
+            case 3 :
+                output.value = tripleDIgitNumMkd(valueInput.value);
+                break;
+            case 4 :
+                output.value = fourDigitsMkd(valueInput.value);
+                break;
+            case 5 :
+                output.value = fiveDigitsMkd(valueInput.value);
+                break;
+            case 6 :
+                output.value = sixDigitsMkd(number);
+                break;
+            case 7 :
+                output.value = sevenDigitsMkd(number);
+                break;
+            case 8 :
+                output.value = eightDigitsMkd(number);
+                break;
+            case 9 :
+                output.value = nineDigitsMkd(number);
+                break;
+            case 10 :
+                output.value = tenDigitsMkd(number);
+                break;
+            default:
+                output.value = `Ве молиме внесете број помегју 0 и 9999999999`
+        }
+    }
+
+}
+
+
