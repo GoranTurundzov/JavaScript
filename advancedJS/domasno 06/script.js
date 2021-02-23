4284866fd8dfab41014fb7242ad2e4d044cabda7
GoranTurundzov <77289944+GoranTurundzov@users.noreply.github.com>
2021-02-23 13:57:53 +0100

    changed to async

--- advancedJS/domasno 06/script.js
+++ advancedJS/domasno 06/script.js
@@ -17,190 +17,161 @@ let navigationService = {
 
 }
 
+document.getElementById("sortTable").addEventListener(`click`, () => {
+    apiServices.getStudentsAsync(navigationService.url);
+    })
+
 
 let apiServices = {
-   
     getStudents: function(url){
         fetch(url)
-        .then(data => data.json())
+        .then(response => response.json())
         .then(students => {
-            // uiService.printStudents(student)
-            uiService.malesStatStartWithBandAverageGrareOver2(students)
-            uiService.femaleOver24Average(students)
-            uiService.manageData(students);
-            uiService.fillCity(students);
-            uiService.fillAge(students);
-            uiService.clearAverage()
-            students.map(student => uiService.fillAverages(student))
-            uiService.averageGrdeAge()
-            navigationService.sortBtn.addEventListener("click" , function(){
-                uiService.clearAverage()
-                
-                 navigationService.output.innerHTML = ""
-                students.map(student => {
-                    
-                    if(navigationService.citySelector.value === "#" && navigationService.ageSelector.value === "#"){
-                        if (student.gender !== navigationService.genderSelector.value && student.averageGrade >= navigationService.gradeSelector.value){
-                            uiService.printStudents(student)
-                            uiService.fillAverages(student)
-                        }
-                    }
-                    else if(navigationService.citySelector.value === "#" && navigationService.ageSelector.value !== "#"){
-                        if (student.gender !== navigationService.genderSelector.value && student.averageGrade >= navigationService.gradeSelector.value && navigationService.ageSelector.value == student.age){
-                            uiService.printStudents(student)
-                            uiService.fillAverages(student)
-
-                        }
-                    }
-                    else if(navigationService.citySelector.value !== "#" && navigationService.ageSelector.value === "#"){
-                        if (student.gender !== navigationService.genderSelector.value && student.averageGrade >= navigationService.gradeSelector.value && navigationService.citySelector.value == student.city){
-                            uiService.printStudents(student) 
-                            uiService.fillAverages(student)
-                        }
-                    }
-                    else if(student.gender !== navigationService.genderSelector.value && student.averageGrade >= navigationService.gradeSelector.value && navigationService.citySelector.value == student.city && navigationService.ageSelector.value == student.age){
-                        uiService.printStudents(student)
-                        uiService.fillAverages(student)
-                       
-                       
-                    }
-                    
-                })
-                    
-                    if(navigationService.output.innerHTML == ""){
-                        navigationService.output.innerHTML = `<tr> <td colspan=7 > There are no entries that match this critera </td> </tr>`
-                    }
-                   
-                    uiService.averageGrdeAge()
-                    
-                })
-                searchBar.addEventListener(`keyup` , function(){
-                   uiService.searchTable()
-                   uiService.averageGrdeAge()
-                })
+            uiService.fillCity(students)
+            uiService.fillAge(students)
+            uiService.printStudents(students)
+            document.getElementById("inputOutput").value = homeworkAssignments.femaleAverageOver24(students)
+            console.log(homeworkAssignments.maleOnBWith2PlusAverage(students));
+            navigationService.studentsBover2.innerHTML = homeworkAssignments.maleOnBWith2PlusAverage(students)
         })
+    },
+   
+    getStudentsAsync: async function(url){
+        let call = await fetch(url)
+        let data = await call.json()
+        let sortGender = await uiService.filterGender(data);
+        let sortCity = await uiService.filterCity(sortGender);
+        let sortGrade = await uiService.filterGrade(sortCity);
+        let sortAge = await uiService.filterAge(sortGrade);
+        await console.log(sortAge)
+        await uiService.printStudents(sortAge);
+        await uiService.printAverages(sortAge);
+        
     }
 }
 
 let uiService = {
-    printStudents: function(student){
-            
-            navigationService.output.innerHTML +=       `
-            <tr>
-                <td> ${student.firstName} </td>
-                <td> ${student.lastName}</td>
-                <td> ${student.email} </td>
-                <td> ${student.gender} </td>
-                <td> ${student.city} </td>
-                <td> ${student.averageGrade} </td>
-                <td> ${student.age} </td>
-            </tr>`
-        
-    },
-    fillCity: function(data){
-        let cities = [];
-        
-      for(city of data){
-          cities.push(city.city)
-      }
-      cities.sort((a, b) => {if(a < b ) return -1})
-     
-      
-      let singleCIties = Array.from(new Set(cities))
     
-   
-      for(value of singleCIties){
-          navigationService.citySelector.innerHTML += `<option value="${value}"> ${value} </option>`
-      }
-    },
-    fillAge: function(data){
-        let ages = [];
-        for(age of data){
-            ages.push(age.age)
-        }
-        let singleAges = Array.from(new Set(ages));
-        singleAges.sort((a , b ) => a - b);
-        for(age of singleAges){
-            navigationService.ageSelector.innerHTML += `<option value="${age}" > ${age} </option>`
-        }
+   filterGender: function(data){
+       let filtered = data.filter((doc) => doc.gender != navigationService.genderSelector.value)
 
-    },
-    manageData: function(data){
-        data.map(student => uiService.printStudents(student))
-       
-    },
-    femaleOver24Average: function(data){
-        let result = data
-        .filter(student => student.age >= 24)
-        .filter(student => student.gender === "Female")
-        .map(student => student.averageGrade )
-        let totalGrades = result.reduce((sum , number) => (sum += number))
-        let average = totalGrades / result.length
-        document.getElementById("inputOutput").value = average;
-    },
-    malesStatStartWithBandAverageGrareOver2: function(data){
-        navigationService.studentsBover2.innerHTML = "";
-        let result = data
-        .filter(student => student.averageGrade >= 2)
-        .filter(student => student.firstName[0] == "B" )
-        .map(student => `
-          <div class="row white padding">
-            <div class="col-md-2"> ${student.firstName}        </div>
-            <div class="col-md-2"> ${student.lastName}          </div>
-            <div class="col-md-2"> ${student.email}             </div>
-            <div class="col-md-2"> ${student.averageGrade}    </div>
-            <div class="col-md-2"> ${student.age}     </div>
-          </div>
-          `
-        )
-        navigationService.studentsBover2.innerHTML = result
-    },
-    averageAge: 0,
-    averageGrade: 0,
-    students: 0,
-    fillAverages: function(student){
-        uiService.averageAge += student.age;
-        uiService.averageGrade += student.averageGrade;
-        uiService.students++;
-    },
-    clearAverage: function(){
-    uiService.averageAge = 0;
-    uiService.averageGrade = 0;
-    uiService.students = 0;
-    },
-    averageGrdeAge: function(){
-        let averageAge = parseFloat(uiService.averageAge / uiService.students).toFixed(1);
-        let averageGrade = parseFloat(uiService.averageGrade / uiService.students).toFixed(2);
-        navigationService.averageAgeSelected.value = averageAge ;
-        navigationService.averageGradeSelected.value = averageGrade ;
-        
-    },
-    searchTable: function() {
-        var input, filter, found, table, tr, td, i, j;
-        input = navigationService.searchBar
-        filter = input.value.toUpperCase();
-        table = document.getElementById("printResult");
-        tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td");
-            for (j = 0; j < td.length; j++) {
-                if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    found = true;
-                }
-            }
-            if (found) {
-                tr[i].style.display = "";
-                found = false;
-               
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
+       return filtered
+   },
+ 
+   filterCity: function(data){
+       if(navigationService.citySelector.value == "#"){
+           return data
+       }
+     let filtered = data.filter((student) => student.city == navigationService.citySelector.value);
+     return filtered     
+   },
+   filterGrade: function(data){
+       let filtered = data.filter(student => student.averageGrade >= navigationService.gradeSelector.value)
+       return filtered
+   },
+   filterAge: function(data){
+       if(navigationService.ageSelector.value == "#"){
+           return data
+       }
+       filtered = data.filter(student => student.age == navigationService.ageSelector.value)
+
+       return filtered
+   },
+   fillCity: function(data){
+    let cities = [];
+    
+  for(city of data){
+      cities.push(city.city)
+  }
+  cities.sort((a, b) => {if(a < b ) return -1})
+ 
+  
+  let singleCIties = Array.from(new Set(cities))
+
+
+  for(value of singleCIties){
+      navigationService.citySelector.innerHTML += `<option value="${value}"> ${value} </option>`
+  }
+},
+fillAge: function(data){
+    let ages = [];
+    for(age of data){
+        ages.push(age.age)
     }
-   
+    let singleAges = Array.from(new Set(ages));
+    singleAges.sort((a , b ) => a - b);
+    for(age of singleAges){
+        navigationService.ageSelector.innerHTML += `<option value="${age}" > ${age} </option>`
+    }
 
+},
+printStudents: function(data){
+    navigationService.output.innerHTML = "";
+        if(data.length === 0){
+            navigationService.output.innerHTML += `
+            <tr> <td colspan=7> There are no results matching this criteria </td> </tr>` 
+        } 
+        data.map(student =>{
+        navigationService.output.innerHTML +=       `
+        <tr>
+            <td> ${student.firstName} </td>
+            <td> ${student.lastName}</td>
+            <td> ${student.email} </td>
+            <td> ${student.gender} </td>
+            <td> ${student.city} </td>
+            <td> ${student.averageGrade} </td>
+            <td> ${student.age} </td>
+        </tr>`
+    })
+},
+averageAge: function(data){
+    let totals =  data.reduce((total , age) => (total += age.age), 0)
+  
+   return (totals / data.length).toFixed(1)
+
+},
+averageGrade: function(data){
+    let totals = data.reduce((total , grade) => (total += grade.averageGrade), 0)
+    return (totals / data.length).toFixed(2)
+},
+printAverages: function(data){
+    navigationService.averageAgeSelected.value = uiService.averageAge(data);
+    navigationService.averageGradeSelected.value = uiService.averageGrade(data);
+}
 }
 
-apiServices.getStudents(navigationService.url);
-uiService.femaleOver24Average
 
+apiServices.getStudents(navigationService.url)
+
+
+let homeworkAssignments = {
+    femaleAverageOver24: function(data){
+        let filtered = data.filter(student =>{
+            if(student.age <= 24 && student.gender == "Female"){
+                return student
+            }
+        })
+        let average = filtered.reduce((total , student) => (total += student.averageGrade) , 0)
+        return (average / filtered.length).toFixed(2)
+    },
+    maleOnBWith2PlusAverage: function(data){
+        let filtered = data.filter(student => {
+            if(student.firstName[0] == "B" && student.averageGrade >= 2 && student.gender == "Male"){
+                return student
+            }
+            
+        })
+       result = filtered.map(student => `
+       <div class="row white padding">
+         <div class="col-md-2"> ${student.firstName}        </div>
+         <div class="col-md-2"> ${student.lastName}          </div>
+         <div class="col-md-4"> ${student.email}             </div>
+         <div class="col-md-2"> ${student.gender}             </div>
+         <div class="col-md-1"> ${student.averageGrade}    </div>
+         <div class="col-md-1"> ${student.age}     </div>
+       </div>
+       `
+     )
+     return result
+    }
+}
