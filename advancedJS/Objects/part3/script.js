23e39bb635acfd2a7d839ae2c47813e3167d8567
GoranTurundzov <77289944+GoranTurundzov@users.noreply.github.com>
2021-03-08 18:57:07 +0100

    getter and setter

--- advancedJS/Objects/part3/script.js
+++ advancedJS/Objects/part3/script.js
@@ -6,18 +6,31 @@ class Animal{
         this.size = size;
         this.isEaten = false;
     }
+    set type(input){
+        input = input.toLowerCase();
+        if(input !== "carnivore" && input !== "herbivore" && input !== "omnivore"){
+            throw new Error(`There is no such type!`)
+        } else {
+            this._type = input
+        }
+    }
+    get type (){
+        return this._type;
+    }
+    
     eat(animal){
         if(animal instanceof Animal){
-        if(this.type === `herbivore` ){
-           console.log( `The animal ${this.name} is a herbivore and does not eat other animals`)
-        } else if(this.type !== `herbivore` && this.size > animal.size){
-            this.isEaten = true ;
-            console.log(` The animal ${this.name} ate ${animal.name}.`)
-        }else if(this.type !== `herbivore` && this.size < animal.size){
-            console.log(`The animal ${this.name} tried to eat  ${animal.name} but it was too large.`)
+            if(animal.isEaten === true){return `This animal is already eaten`}
+        if(this._type === `herbivore` ){
+           return  `The animal ${this.name} is a herbivore and does not eat other animals`
+        } else if(this._type !== `herbivore` && this.size > animal.size ){
+            animal.isEaten = true ;
+           return` The animal ${this.name} ate ${animal.name}.`
+        }else if(this.type !== `herbivore` && (this.size * 2) <= animal.size){
+            return `The animal ${this.name} tried to eat  ${animal.name} but it was too large.`
         }
     } else{
-        console.log(`The animal ${this.name} is eating ${animal}`)
+        return `The animal ${this.name} is eating ${animal}`
     }
 
 }
@@ -25,13 +38,16 @@ class Animal{
 }
 let grass = `grass`;
 let pig = new Animal(`Chriss P. Bacon` ,`herbivore` , 5 , 100)
-console.log(pig)
 let wolf = new Animal(`Wolf Mc'volf` , `carnivore` , 6 , 150 )
-console.log(wolf)
 let cow = new Animal(`Moosky` , `herbivore` , 3 , 300)
-console.log(cow)
-wolf.eat(pig)
+console.log(pig)
 console.log(wolf)
-pig.eat(wolf)
-wolf.eat(cow)
-pig.eat(grass)
+console.log(cow)
+console.log(wolf.eat(pig))
+console.log(wolf)
+console.log(pig.eat(wolf))
+console.log(wolf.eat(cow))
+console.log(pig)
+console.log(pig.eat(grass))
+
+let tree = new Animal(`A tree` , `tree` , 100 , 500)
